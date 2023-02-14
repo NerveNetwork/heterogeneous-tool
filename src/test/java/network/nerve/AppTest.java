5d6b688abbcf36aacab95ecfe9f02f485ce13eec
zhouvivi1985 <zoro.vivi1985@gmail.com>
2023-02-14 18:05:17 +0800

    1

--- src/test/java/network/nerve/AppTest.java
+++ src/test/java/network/nerve/AppTest.java
@@ -3,6 +3,9 @@ package network.nerve;
 import network.nerve.heterogeneous.utils.JsonRpcUtil;
 import network.nerve.heterogeneous.utils.RpcResult;
 import org.junit.Test;
+import org.web3j.ens.EnsResolver;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
 
 import java.util.Arrays;
 
@@ -26,5 +29,16 @@ public class AppTest {
         String method = "eth_getBlockByNumber";
         RpcResult result = JsonRpcUtil.requestForMetaMask(url, "BSC", method, Arrays.asList("0x1", false));
         System.out.println(result.toString());
+    }
+
+
+    @Test
+    public void testEns() {
+        String apiUrl = "https://eth-mainnet.public.blastapi.io";
+        Web3j web3j = Web3j.build(new HttpService(apiUrl));
+        EnsResolver ens = new EnsResolver(web3j, 300);
+        System.out.println(ens.resolve("000.eth"));
+
+        //byte[] nameHash = NameHash.nameHashAsBytes("000.eth");
     }
 }
